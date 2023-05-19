7c7a65b3bf336ebfb8b7d5350a612af68b603272
nschutz <nschutz@student.42wolfsburg.de>
2023-05-19 12:38:45 +0200

    feat: added finished function strlcat

--- libft.h
+++ libft.h
@@ -29,6 +29,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_strlen(const char *s);
 int		ft_strcmp(const char *s1, const char *s2, size_t n);
 char	ft_strstr(const char *haystack, const char *needle);
